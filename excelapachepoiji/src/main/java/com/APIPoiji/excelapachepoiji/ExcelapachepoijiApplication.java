caa91a412f5758585939178435b94bf06c494dcd
Anderson Martins <BlackCode77.1980@gmail.com>
2024-02-12 00:34:14 -0300

    [ Poiji ] - estudos para extração de relatórios em excel

---
+++ excelapachepoiji/src/main/java/com/APIPoiji/excelapachepoiji/ExcelapachepoijiApplication.java
@@ -0,0 +1,13 @@
+package com.APIPoiji.excelapachepoiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelapachepoijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelapachepoijiApplication.class, args);
+	}
+
+}
---
+++ excelapachepoiji/src/test/java/com/APIPoiji/excelapachepoiji/ExcelapachepoijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIPoiji.excelapachepoiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelapachepoijiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
